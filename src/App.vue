ab3e4bc371609523ebc87ecccacda383e977a6db
Franco Profeti <fprofeti98@gmail.com>
2021-12-23 10:03:17 -0300

    tauri + tailwind 3

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <nav>
+  <nav class="bg-red-500">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
 </template>
 
-<style>
+<style lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="about bg-red-500 font-bold">
     <h1>This is an about page</h1>
   </div>
 </template>
